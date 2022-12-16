b952dec1c1acafd77999f5dd650af1eed9535753
alerams <84053996+alerams@users.noreply.github.com>
2022-12-16 14:40:15 -0800

    Borrado de boton tooltip

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1812,12 +1812,7 @@ const renderBitacora = async (divContent) => {
   </div> 
                           </div>
                           </div>
-                          <button type="button" class="btn btn-secondary"
-        data-bs-toggle="tooltip" data-bs-placement="top"
-        data-bs-custom-class="custom-tooltip"
-        data-bs-title="This top tooltip is themed via CSS variables.">
-  Custom tooltip 
-</button>
+                        
 
 
 
